a6238cd96e794d3c1798fdd808c34d39c1b10801
Marsad1998 <marsadakbar1@gmail.com>
2025-09-11 16:12:20 +0500

    fix the marketing domain

--- dist/assets/Error-c4cba967.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./index-d146d5df.js";const e=()=>r("div",{children:r("h1",{children:"Error"})});export{e as default};
--- src/pages/PermissionRouter/PermissionRouter.tsx
+++ src/pages/PermissionRouter/PermissionRouter.tsx
@@ -7,9 +7,9 @@ const PermissionRouter = () => {
   const [permissions, setPermissions] = useState<any>([]);
 
   useEffect(() => {
-    // Load permissions from localStorage or your auth system
-    const userPermissions = JSON.parse(localStorage.getItem('userPermissions') || '[]');
-    setPermissions(userPermissions);
+    const storedPermissions = JSON.parse(localStorage.getItem('permissions') || '[]');
+    setPermissions(storedPermissions);
+    console.log("ererdffd="+storedPermissions)
   }, []);
 
   return permissions.includes('manage marketing') ? <AnalyticsDashboard /> : <Dashboardanalysis />;
