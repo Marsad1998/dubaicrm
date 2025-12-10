a7c1f09e51db10b22bd1340ba8ade6d94a44af5a
Marsad1998 <marsadakbar1@gmail.com>
2025-12-10 17:25:07 +0500

    fix the code

--- src/components/BaseUrl.tsx
+++ src/components/BaseUrl.tsx
@@ -2,7 +2,7 @@ export const getBaseUrl = () => {
   const currentUrl = window.location.href;
   if (currentUrl.includes('localhost')) {
     // return 'http://10.99.1.40:8000/api';
-    return 'http://10.255.254.61:8000/api';
+    // return 'http://10.255.254.61:8000/api';
     return 'https://newcrmbackend.ddev.site/api';
     // return 'http://10.99.1.53:8000/api';
   } else if (currentUrl.includes('testcrm.leadshub.ae')) {
