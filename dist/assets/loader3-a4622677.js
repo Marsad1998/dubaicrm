97e3a00feb0e77de315706f0034a04399b27214b
idrees-office <idreesoffice9012@gmail.com>
2026-02-27 12:04:31 +0400

    update codes

---
+++ dist/assets/loader3-a4622677.js
@@ -0,0 +1,1 @@
+import{b as r}from"./index-4fca83d7.js";const a=()=>r("div",{className:"grid place-content-center min-h-[300px]",children:r("div",{className:"animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-primary"})});export{a as L};
