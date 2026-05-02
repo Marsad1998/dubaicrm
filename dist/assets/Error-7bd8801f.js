9012e2b74fc80b4ddbe74e56228e852919cb1b99
Marsad <marsadakbar@gmail.com>
2026-05-02 17:35:58 +0500

    fix the code

--- dist/assets/Error-7bd8801f.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./index-060daddf.js";const e=()=>r("div",{children:r("h1",{children:"Error"})});export{e as default};
--- src/components/BaseUrl.tsx
+++ src/components/BaseUrl.tsx
@@ -1,17 +1,9 @@
 export const getBaseUrl = () => {
   const currentUrl = window.location.href;
   if (currentUrl.includes('localhost')) {
-    // return 'http://newcrmbackend.ddev.site:33000/api';
-    return 'https://backend.leadshub.ae/api';
-    // return 'http://10.255.254.61:8000/api';
-    return 'http://newcrmbackend.ddev.site/api';
     return 'http://localhost:8000/api';
-  } else if (currentUrl.includes('testcrm.leadshub.ae')) {
-    return 'https://testcrmbackend.leadshub.ae/api';
-  } else if (currentUrl.includes('leadshub.ae')) {
-    return 'https://backend.leadshub.ae/api';
-  } else if (currentUrl.includes('evernest.online')) {
-      return 'https://backend.leadshub.ae/api';
+  } else if (currentUrl.includes('business.sandbox.pk')) {
+    return 'https://business.sandbox.pk/api';
   } else {
     return 'http://10.99.1.93:8000/api';
   }
